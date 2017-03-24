aab34a917992cb7cd296177916077ce955650ff5
jraniero <jose.raniero@gmail.com>
2017-03-24 21:37:26 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# capstone
-# capstoneproject
+
+# Capstoneproject
+
+##Purpose
+This repository holds the files of the Coursera Data Science Specialization CapStone project.
+
+##Shiny App
+Under TextPredictor you will find the files for the Shiny App
+The Shiny app is available here: https://jraniero.shinyapps.io/TextPredictor/
+
+##Preprocessing scripts
+In the main directory you will find
+- The Report.Rmd file that shows the exploratory data analysis and how the training data was obtained from the .txt project files
+- The W3StoreNGrams.R script, that was used to create the n-gram dictionnary used by the shiny app
+- The make_test_set.R script, used to create the test set from the project .txt files
+- The test_perf.R script, used to measure the performance of the prediction algorithm
+- The Analysis_Performance_Results.Rmd, that reports on the performance of the algorithm
+- Different unix scripts used for exploratory analysis, sampling and cleanup
+
+##Environment
+R version 3.3.2 (2016-10-31)
+Platform: x86_64-apple-darwin13.4.0 (64-bit)
+Running under: OS X El Capitan 10.11.4
+
+locale:
+[1] fr_BE.UTF-8/fr_BE.UTF-8/fr_BE.UTF-8/C/fr_BE.UTF-8/fr_BE.UTF-8
+
+attached base packages:
+[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+[1] wordcloud_2.5      RColorBrewer_1.1-2 shiny_0.14.2       data.table_1.9.6  
+[5] SnowballC_0.5.1    tm_0.6-2           NLP_0.1-9          stringr_1.1.0     
+
+loaded via a namespace (and not attached):
+ [1] Rcpp_0.12.5     knitr_1.14      magrittr_1.5    xtable_1.8-2    R6_2.1.2       
+ [6] tools_3.3.2     parallel_3.3.2  htmltools_0.3.5 yaml_2.1.13     rprojroot_1.2  
+[11] digest_0.6.9    RJSONIO_1.3-0   bitops_1.0-6    RCurl_1.95-4.8  rsconnect_0.7  
+[16] evaluate_0.9    slam_0.1-40     mime_0.4        rmarkdown_1.3   stringi_1.1.2  
+[21] backports_1.0.5 jsonlite_1.2    httpuv_1.3.3    chron_2.3-47 
--- W3StoreNGrams.R
+++ W3StoreNGrams.R
@@ -1,4 +1,8 @@
-knitr::opts_chunk$set(echo = TRUE)
+# This script will create the dictionnary used by the shiny app
+# For that, the sampled text (see Report.Rmd) is used to create the occurrences of n-grams
+# As the script takes considerable time to process, intermediate results are saved
+# This allows for resuming the process should it be necessary
+
 library(stringr)
 library(tm)
 library(SnowballC)
@@ -15,7 +19,7 @@ removeTwitterHandles <- function(x) gsub("@\\S+", "", x)
 #file="en_US.blogs.txt"
 myText<-Corpus(DirSource(datapath,pattern="*"))
 
-
+#Preprocess text
 myText <- tm_map(myText, removeNumbers)
 myText<-tm_map(myText,content_transformer(removeURL))
 myText<-tm_map(myText,content_transformer(removeHashTags))
@@ -29,7 +33,7 @@ myText <- tm_map(myText, removeWords, stopwords("english"))
 myText<-tm_map(myText, stemDocument, language = "english")  
 myText <- tm_map(myText ,stripWhitespace)
 
-
+#Calculate from 1-gram to 5-grams
 freq<-data.frame(word=character(0),frequency=character(0),words=numeric(0))
 for(ngrams in c(1:5)){
   print(paste("Doing ",ngrams," ngrams"))
@@ -47,13 +51,15 @@ saveRDS(freq,"ngrams.rds") #Save intermediate result
 
 #freq<-readRDS("ngrams.rds")
 
+#Aggregate the results of the different files
 freq_agg<-aggregate(frequency~.,data=freq,FUN=sum)
 
 saveRDS(freq_agg,"ngrams_agg.rds")
 
 #freq_agg<-readRDS("ngrams_agg.rds")
 
-freq_agg$word<-unlist(lapply(lapply(strsplit(as.character(freq_agg$word)," "),rev),paste,collapse=" "))#Reverse order of words in each phrase
+#Reverse the order of words in each phrase
+freq_agg$word<-unlist(lapply(lapply(strsplit(as.character(freq_agg$word)," "),rev),paste,collapse=" "))
 
 saveRDS(freq_agg,"ngrams_agg_rev.rds")
 
@@ -61,17 +67,21 @@ rm(list=ls()) #Cleanup
 
 freq_clean<-data.table(readRDS("ngrams_agg_rev.rds"))
 
+#Write to csv for cleanup with unix tools
 write.csv(freq_clean,"ngram.csv",row.names=FALSE)
 
+#Add necessary empty words for 3, 4 and 5-grams
 system("cat ngram.csv | sed -e 's/ /\",\"/g' | sed -e 's/\",1/\",\"\",\"\",\"\",\"\",1/' | sed -e 's/\",2/\",\"\",\"\",\"\",2/' | sed -e 's/\",3/\",\"\",\"\",3/' | sed -e 's/\",4/\",\"\",4/' > ngram_words.csv") #Split into a columnd per word
 
 ngrams<-data.table(read.csv("ngram_words.csv",skip=1,col.names=c("last_word","word_1","word_2","word_3","word_4","words","freq")))
 
+#Order in decreasing frequency and number of words order
 ngrams<-ngrams[order(-words,-freq)]
 
 saveRDS(ngrams,"ngrams_final.rds")
 
 #Create smaller version for the shiny app
+#By just removing the items appearing only once
 ngrams_app<-ngrams[ngrams$freq>=2,]
 
 saveRDS(ngrams_app,"ngrams_app.rds")
